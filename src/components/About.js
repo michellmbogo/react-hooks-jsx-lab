ecdaf9d09b2a31086d55b5b23cffed444edb1e1f
Michell Wambui Mbogo <mishie254@gmail.com>
2025-01-19 16:40:11 +0300

    react hooks

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,14 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return <div id= "about">
+    <h2>About Me</h2>
+    <p>Am so passionate in programming I hope to be a software developer one day</p>
+    <img
+    src={image}
+    alt="I made this"
+    />
+  </div>;
 }
 
 export default About;
